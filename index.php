9a924b3d67f931d41cf1f46c63a3cea6a9d57252
wojci08 <wojciech.siemienik@gmail.com>
2026-02-01 17:44:07 +0100

    zmiana wyglądu

--- index.php
+++ index.php
@@ -13,35 +13,24 @@ if ($conn->connect_error) {
 // Pobierz dane demograficzne
 $sql = 'SELECT * FROM demografia';
 $result = $conn->query($sql);
-?>
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <title>Dane demograficzne Polski</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Dane demograficzne Polski</h1>
-    <table>
-        <tr>
-            <th>Rok</th>
-            <th>Liczba ludności</th>
-            <th>Przyrost naturalny</th>
-        </tr>
-        <?php if ($result && $result->num_rows > 0): ?>
-            <?php while($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td><?= htmlspecialchars($row['rok']) ?></td>
-                    <td><?= htmlspecialchars($row['liczba_ludnosci']) ?></td>
-                    <td><?= htmlspecialchars($row['przyrost_naturalny']) ?></td>
-                </tr>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <tr><td colspan="3">Brak danych w bazie.</td></tr>
-        <?php endif; ?>
-    </table>
-</body>
-</html>
-<?php
+
+// Generuj HTML tabel z danymi
+$tableContent = '';
+if ($result && $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $tableContent .= '<tr>';
+        $tableContent .= '<td>' . htmlspecialchars($row['rok']) . '</td>';
+        $tableContent .= '<td>' . htmlspecialchars($row['liczba_ludnosci']) . '</td>';
+        $tableContent .= '<td>' . htmlspecialchars($row['przyrost_naturalny']) . '</td>';
+        $tableContent .= '</tr>';
+    }
+} else {
+    $tableContent = '<tr><td colspan="3">Brak danych w bazie.</td></tr>';
+}
+
+// Załaduj szablon HTML
+$html = file_get_contents('public_html/index.html');
+$html = str_replace('{CONTENT}', $tableContent, $html);
+echo $html;
+
 $conn->close();
