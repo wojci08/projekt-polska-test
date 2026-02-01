361e5bc03161447a0fc7a46ec00f4a8e52a39b32
wojci08 <wojciech.siemienik@gmail.com>
2026-02-01 18:42:49 +0100

    linki przez vhosts

--- index.php
+++ index.php
@@ -10,22 +10,33 @@ if ($conn->connect_error) {
     die('Błąd połączenia z bazą danych: ' . $conn->connect_error);
 }
 
-// Pobierz dane demograficzne
-$sql = 'SELECT * FROM demografia';
-$result = $conn->query($sql);
+// Pobierz aktualną stronę z URL-a
+$page = isset($_GET['page']) ? $_GET['page'] : 'demografia';
 
-// Generuj HTML tabel z danymi
+// Generuj zawartość na podstawie wybranej strony
 $tableContent = '';
-if ($result && $result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $tableContent .= '<tr>';
-        $tableContent .= '<td>' . htmlspecialchars($row['rok']) . '</td>';
-        $tableContent .= '<td>' . htmlspecialchars($row['liczba_ludnosci']) . '</td>';
-        $tableContent .= '<td>' . htmlspecialchars($row['przyrost_naturalny']) . '</td>';
-        $tableContent .= '</tr>';
+
+if ($page === 'demografia') {
+    // Pobierz dane demograficzne
+    $sql = 'SELECT * FROM demografia';
+    $result = $conn->query($sql);
+
+    // Generuj HTML tabel z danymi
+    if ($result && $result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $tableContent .= '<tr>';
+            $tableContent .= '<td>' . htmlspecialchars($row['rok']) . '</td>';
+            $tableContent .= '<td>' . htmlspecialchars($row['liczba_ludnosci']) . '</td>';
+            $tableContent .= '<td>' . htmlspecialchars($row['przyrost_naturalny']) . '</td>';
+            $tableContent .= '</tr>';
+        }
+    } else {
+        $tableContent = '<tr><td colspan="3">Brak danych w bazie.</td></tr>';
     }
-} else {
-    $tableContent = '<tr><td colspan="3">Brak danych w bazie.</td></tr>';
+} elseif ($page === 'miasta') {
+    $tableContent = '<p style="text-align: center; margin-top: 30px;">Zawartość o miastach będzie wkrótce dostępna...</p>';
+} elseif ($page === 'wartozobaczyc') {
+    $tableContent = '<p style="text-align: center; margin-top: 30px;">Zawartość o atrakcjach turystycznych będzie wkrótce dostępna...</p>';
 }
 
 $sql = 'SELECT * FROM linki';
