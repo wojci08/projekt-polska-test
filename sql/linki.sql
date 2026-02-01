f1660f8db873be0618e6761d6dbdd85da57c7b73
wojci08 <wojciech.siemienik@gmail.com>
2026-02-01 18:17:31 +0100

    dynamiczne menu

---
+++ sql/linki.sql
@@ -0,0 +1,5 @@
+-- Przykładowe dane
+INSERT INTO linki (nazwa, url) VALUES
+('Demografia', 'http://projektpolska.pl/demografia'),
+('Miasta', 'http://projektpolska.pl/miasta'),
+('Warto zobaczyc', 'http://projektpolska.pl/wartozobaczyc');
